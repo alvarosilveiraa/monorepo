7a1c2ce1aaee41bcc1896d2bff2a53ef39436885
Alvaro Silveira <alvarosilveiraa@gmail.com>
2021-12-12 18:56:17 -0300

    Adicionando env ao services

--- packages/services/src/index.ts
+++ packages/services/src/index.ts
@@ -1,4 +1,6 @@
 import {server} from './config';
 import './routes';
 
-server.listen(8080, () => console.log('listening on http://locahost:8080'));
+const {PORT = 80} = process.env;
+
+server.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
